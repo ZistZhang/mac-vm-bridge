3ea90de31d8593c01def5be23dacd0928492c119
ZistZhang <2558964112@qq.com>
2025-10-11 22:11:06 +0800

    feat(qemu-up): add MVB_COMPAT (IDE+e1000) fallback mode for legacy VMs

--- scripts/macos/qemu-up.sh
+++ scripts/macos/qemu-up.sh
@@ -16,18 +16,31 @@ probe_port() {
   echo "$p"
 }
 SSH_PORT=$(probe_port "$SSH_PORT")
-
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
-sudo --preserve-env=OBJC_DISABLE_INITIALIZE_FORK_SAFETY \
-  "$QEMU_BIN" \
-  -accel tcg,thread=multi -cpu max -smp "$SMP" -m "$MEM" \
-  -drive file="$DISK",format=qcow2,if=virtio,cache=writeback \
-  -netdev user,id=mgmt,hostfwd=tcp:127.0.0.1:"$SSH_PORT"-:22 \
-  -device virtio-net-pci,netdev=mgmt,mac=52:54:00:22:33:45 \
-  -netdev vmnet-bridged,id=biz,ifname="$BR_IF" \
-  -device virtio-net-pci,netdev=biz,mac=52:54:00:22:33:44 \
-  -vga virtio -display default,show-cursor=on \
-  -daemonize -pidfile vm.pid -qmp unix:qmp.sock,server,nowait \
-  -D qemu.log -msg timestamp=on
+if [[ "${MVB_COMPAT:-0}" == "1" ]]; then
+  sudo --preserve-env=OBJC_DISABLE_INITIALIZE_FORK_SAFETY \
+    "$QEMU_BIN" \
+    -accel tcg,thread=multi -cpu max -smp "$SMP" -m "$MEM" \
+    -drive file="$DISK",format=qcow2,if=ide,cache=writeback \
+    -netdev user,id=mgmt,hostfwd=tcp:127.0.0.1:"$SSH_PORT"-:22 \
+    -device e1000,netdev=mgmt,mac=52:54:00:22:33:45 \
+    -netdev vmnet-bridged,id=biz,ifname="$BR_IF" \
+    -device e1000,netdev=biz,mac=52:54:00:22:33:44 \
+    -vga std -display default,show-cursor=on \
+    -daemonize -pidfile vm.pid -qmp unix:qmp.sock,server,nowait \
+    -D qemu.log -msg timestamp=on
+else
+  sudo --preserve-env=OBJC_DISABLE_INITIALIZE_FORK_SAFETY \
+    "$QEMU_BIN" \
+    -accel tcg,thread=multi -cpu max -smp "$SMP" -m "$MEM" \
+    -drive file="$DISK",format=qcow2,if=virtio,cache=writeback \
+    -netdev user,id=mgmt,hostfwd=tcp:127.0.0.1:"$SSH_PORT"-:22 \
+    -device virtio-net-pci,netdev=mgmt,mac=52:54:00:22:33:45 \
+    -netdev vmnet-bridged,id=biz,ifname="$BR_IF" \
+    -device virtio-net-pci,netdev=biz,mac=52:54:00:22:33:44 \
+    -vga virtio -display default,show-cursor=on \
+    -daemonize -pidfile vm.pid -qmp unix:qmp.sock,server,nowait \
+    -D qemu.log -msg timestamp=on
+fi
 
 echo "[+] QEMU started. SSH: 127.0.0.1:$SSH_PORT; QMP: qmp.sock; PID: $(cat vm.pid)"
